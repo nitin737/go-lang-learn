68ce05bbc8db47db89f11a0cedd0704497ef2b32
Nitin <nitin127001@gmail.com>
2022-10-02 16:32:48 +0000

    day 1 : firstapp - variables in go lang

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nitin737/firstapp
+
+go 1.19
---
+++ src/github.com/nitin737/firstapp/Main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conferense"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	//ask user for their name
+
+	fmt.Scan(&userName)
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+}
