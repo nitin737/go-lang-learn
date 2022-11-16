437137e6551c43700270c221a532153bd8293c16
Nitin <nitin127001@gmail.com>
2022-11-16 17:37:35 +0000

    bookstore

---
+++ bookstore/cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com.gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/nitin/bookstore/pkg/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoresRoutes(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r))
+	
+}
+
